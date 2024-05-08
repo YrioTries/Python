ed703e10bee20c9879bd44a7c4e544e0d9c94d81
Dmitry <121174596+YrioTries@users.noreply.github.com>
2024-05-08 11:06:54 +0400

    Update графика4.py

--- Second_year/Second_semester/Computer_graphics/графика4.py
+++ Second_year/Second_semester/Computer_graphics/графика4.py
@@ -24,7 +24,24 @@ def perspective(x, y, z):
     return persp_coordinates[0], persp_coordinates[1]
 
 
-def drawtriangle(x0, y0, z0, x1, y1, z1, x2, y2, z2, cosI0, cosI1, cosI2):
+def drawtriangle(
+    x0,
+    y0,
+    z0,
+    x1,
+    y1,
+    z1,
+    x2,
+    y2,
+    z2,
+    cosI0,
+    cosI1,
+    cosI2,
+    texture_pic,
+    vt_0,
+    vt_1,
+    vt_2,
+):
 
     p_x0, p_y0 = perspective(x0, y0, z0)
     p_x1, p_y1 = perspective(x1, y1, z1)
@@ -50,9 +67,20 @@ def drawtriangle(x0, y0, z0, x1, y1, z1, x2, y2, z2, cosI0, cosI1, cosI2):
                 I = cosI0 * lambda0 + cosI1 * lambda1 + cosI2 * lambda2
                 if I > 0:
                     I = 0
+                color_test = texture_pic[
+                    int(
+                        1024
+                        * (vt_0[1] * lambda0 + vt_1[1] * lambda1 + vt_2[1] * lambda2)
+                    )
+                ][
+                    int(
+                        1024
+                        * (vt_0[0] * lambda0 + vt_1[0] * lambda1 + vt_2[0] * lambda2)
+                    )
+                ]
                 color = -255 * I
                 if tempZ < z_buf[u][v]:
-                    image3d[v, u] = color
+                    image3d[v, u] = color_test
                     z_buf[u][v] = tempZ
 
 
@@ -107,6 +135,8 @@ vertex_list = []
 edge_list = []
 polygons_list = []
 texture_list = []
+texture_nums = []
+vt_list = []
 
 f = open("model_1.obj")
 
@@ -117,11 +147,13 @@ for s in f:
     if splitted[0] == "f":
         for i in splitted[1:4]:
             edge_list.append(int(i.split("/")[0]))
+            texture_nums.append(int(i.split("/")[1]))
     if splitted[0] == "vt":
         texture_list.append([float(x) for x in splitted[1:3]])
 
 
 edge_list = [edge_list[i : i + 3] for i in range(0, len(edge_list), 3)]
+texture_nums = [texture_nums[i : i + 3] for i in range(0, len(texture_nums), 3)]
 
 vertex_normals = np.zeros((len(vertex_list), 3))
 
@@ -144,6 +176,22 @@ for j in range(len(edge_list)):
         (vertex_list[edge_list[j][2] - 1][2]),
     )
 
+    vt_0, vt_1, vt_2 = (
+        [
+            texture_list[texture_nums[j][0] - 1][0],
+            texture_list[texture_nums[j][0] - 1][1],
+        ],
+        [
+            texture_list[texture_nums[j][1] - 1][0],
+            texture_list[texture_nums[j][1] - 1][1],
+        ],
+        [
+            texture_list[texture_nums[j][2] - 1][0],
+            texture_list[texture_nums[j][2] - 1][1],
+        ],
+    )
+
+    vt_list.append([vt_0, vt_1, vt_2])
     polygons_list.append([x0, y0, z0, x1, y1, z1, x2, y2, z2])
 
     polygons_normals = calculate_normal_polygon(x0, y0, z0, x1, y1, z1, x2, y2, z2)
@@ -159,6 +207,8 @@ light_list = []
 
 for i in range(len(vertex_normals)):
     light_list.append(calculate_light(vertex_normals[i], np.array([0, 0, 1])))
+
+texture_pic = np.array(ImageOps.flip(Image.open("bunny.jpg")))
 
 j = 0
 for i in polygons_list:
@@ -183,10 +233,14 @@ for i in polygons_list:
             light_list[edge_list[j][0] - 1],
             light_list[edge_list[j][1] - 1],
             light_list[edge_list[j][2] - 1],
+            texture_pic,
+            vt_list[j][0],
+            vt_list[j][1],
+            vt_list[j][2],
         )
     j += 1
 
 img = Image.fromarray(image3d, mode="RGB")
 img = ImageOps.flip(img)
 img.show()
-# img.save("lab4_pic1.png")
+img.save("final.png")
